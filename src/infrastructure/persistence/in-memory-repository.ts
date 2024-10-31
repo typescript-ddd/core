f3fd672c01bff0cfa02b91c8e4dde91428e11809
John Robinson <johnrob1880@gmail.com>
2024-10-31 05:42:35 -0500

    Updated in-memory repository to work with entity id values

--- src/infrastructure/persistence/in-memory-repository.ts
+++ src/infrastructure/persistence/in-memory-repository.ts
@@ -1,5 +1,6 @@
 import { Repository } from "../../domain/model/repository";
 import { Criteria } from "../../domain/criteria/criteria";
+import { EntityId } from "../../domain";
 
 interface EntityData<TId> {
   id: TId;
@@ -20,19 +21,37 @@ export class InMemoryRepository<TId, TData extends EntityData<TId>>
     return Promise.resolve(Array.from(this.data.values()));
   }
   getById(id: TId): Promise<TData | undefined> {
+    if (id instanceof EntityId) {
+      const entity = this.data.get(id.value as TId);
+      return Promise.resolve(entity);
+    }
     const entity = this.data.get(id);
     return Promise.resolve(entity);
   }
   save(entity: TData): Promise<void> {
+    if (entity.id instanceof EntityId) {
+      this.data.set(entity.id.value as TId, entity);
+      return Promise.resolve();
+    }
     this.data.set(entity.id, entity);
     return Promise.resolve();
   }
-  remove(entity: TId): Promise<void> {
-    this.data.delete(entity);
+  remove(entityId: TId): Promise<void> {
+    if (entityId instanceof EntityId) {
+      this.data.delete(entityId.value as TId);
+    } else {
+      this.data.delete(entityId);
+    }
     return Promise.resolve();
   }
 
   protected seed(data: TData[]): void {
-    data.forEach((entity) => this.data.set(entity.id, entity));
+    data.forEach((entity) => {
+      if (entity.id instanceof EntityId) {
+        this.data.set(entity.id.value as TId, entity);
+        return;
+      }
+      this.data.set(entity.id, entity);
+    });
   }
 }

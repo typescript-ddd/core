72b15241ecf18477f7cad20ce338d88ebbfa7c7b
John Robinson <johnrob1880@gmail.com>
2024-11-06 06:40:46 -0600

    Updated command and query actions

--- src/presentation/controller/command-action.ts
+++ src/presentation/controller/command-action.ts
@@ -1,8 +1,8 @@
 import { CommandBus } from "../../domain/bus/command";
 
-export abstract class CommandAction<TRequest = any, TResponse = any, TContext = any> {
+export abstract class CommandAction<TReturn, TRequest = any> {
   method: string = "POST";
   path: string = "/";
   constructor(protected readonly commandBus: CommandBus) {}
-  abstract execute(req: TRequest, res: TResponse, ctx: TContext): Promise<void>;
+  abstract execute(req: TRequest): Promise<TReturn>;
 }
--- src/presentation/controller/query-action.ts
+++ src/presentation/controller/query-action.ts
@@ -1,8 +1,8 @@
 import { QueryBus } from "../../domain/bus/query";
 
-export abstract class QueryAction<TRequest = any, TResponse = any, TContext = any> {
+export abstract class QueryAction<TReturn = any, TRequest = any> {
   method: string = "GET";
   path: string = "/";
   constructor(protected readonly queryBus: QueryBus) {}
-  abstract execute(req: TRequest, res: TResponse, ctx: TContext): Promise<void>;
+  abstract execute(req: TRequest): Promise<TReturn>;
 }
